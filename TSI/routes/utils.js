1b955ffc9c937cea6e2f131487f01edd43d6767f
unknown <pelmeshka1110@gmail.com>
2020-12-24 11:03:06 +0300

    pre-release

--- TSI/routes/utils.js
+++ TSI/routes/utils.js
@@ -2,18 +2,22 @@
 //const router = express.Router();
 
 const path = require("path");
+const fs = require('fs');
 
 const uploadsDir = path.join(__dirname, '..', 'uploads/');
 const ratesDBPath = path.join(__dirname, '..', 'rates.db');
 
-const fileExtensionStr = /(?:\.([^.]+))?$/;
+//const fileExtensionStr = /(?:\.([^.]+))?$/;
 
 
-
+const HasFile = (name)=>{
+    return fs.existsSync(uploadsDir+name);
+}
 
 
 module.exports = {
     uploadsDir,
     ratesDBPath,
-    fileExtensionStr
+    //fileExtensionStr,
+    HasFile
 };
